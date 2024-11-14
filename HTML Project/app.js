e15b1bf14869f2a44a3ed39416d6248590d39bf0
Dalton Roberts <drobe157@students.kennesaw.edu>
2024-11-13 21:37:51 -0500

    Project Milestone 3

--- HTML Project/app.js
+++ HTML Project/app.js
@@ -2,10 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
+const ejs = require('ejs');
 
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.use(express.static(path.join(__dirname, 'public'))); // set public folder as base directory for stylesheets and pictures
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/HomePage.html'); // send HomePage on base GET request
@@ -27,21 +31,25 @@ app.post('/submit-form', (req, res) => {
     const username = req.body.name; // access form data
     const email = req.body.email;
     if (username == "" || email == "") {
-        res.send("Please enter both a username and email"); // if username or email empty 
+        //     res.send("Please enter both a username and email"); // if username or email empty 
+        res.render('SubmissionPage', { header: 'Error!', message: 'Please enter a valid username and email' });
+    } else if (!isEmailValid(email)) {
+        //res.send(`The email address ${email} is not a valid email, please enter a valid email address`); // if email doesn't validate
+        res.render('SubmissionPage', { header: 'Error!', message: `The email address ${email} is not a valid email, please enter a valid email address` });
+    } else {
+        //es.send(`Thank you for contacting us, ${username}! We will get back you at ${email}`); // if username and email are valid this is successful
+        res.render('SubmissionPage', { header: 'Submission Complete!', message: `Thank you for contacting us, ${username}! We will get back you at ${email}` });
     }
 
-    if (!isEmailValid(email)) {
-        res.send(`The email address ${email} is not a valid email, please enter a valid email address`); // if email doesn't validate
-    }
-
-    res.send(`Thank you for contacting us, ${username}! We will get back you at ${email}`); // if username and email are valid this is successful
+    
 });
+
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
 
-const emailRegex = /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)$/;
+const emailRegex = /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)$/
 
 
 function isEmailValid(email) {
